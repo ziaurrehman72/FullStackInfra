3e998b0fb3f49d0eca11f89e1037af083ab34b20
ziaurrehman72 <ziaurrehman72@yahoo.com>
2021-04-10 08:04:19 +0200

    added input validations

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,21 +2,37 @@ variable "tenant_id" {
   type        = string
   description = "Tenant ID for the Azure Provider"
   default     = "__tenantId__"
+  validation {
+    condition     = length(var.tenant_id) > 10 && length(var.tenant_id) < 400
+    error_message = "The tenant_id value must be a valid Tenant id."
+  }
 }
 variable "client_id" {
   type        = string
   description = "Client ID/Username for the Azure Provider"
   default     = "__clientId__"
+  validation {
+    condition     = length(var.client_id) > 10 && length(var.client_id) < 400
+    error_message = "The client_id value must be a valid Client id."
+  }
 }
 variable "client_secret" {
   type        = string
   description = "Client Secret for the Azure Provider"
   default     = "__clientSecret__"
+  validation {
+    condition     = length(var.client_secret) > 8 && length(var.client_secret) < 200
+    error_message = "The client_secret value must be a valid Client secret."
+  }
 }
 variable "subscription_id" {
   type        = string
   description = "Subscription ID for the Azure Provider"
   default     = "__subscriptionId__"
+  validation {
+    condition     = length(var.subscription_id) > 8 && length(var.subscription_id) < 200
+    error_message = "The client_secret value must be a valid Client secret."
+  }
 }
 
 
